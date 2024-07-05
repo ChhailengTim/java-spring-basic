87b12d045120cf97f3f8aa4ba0c154d1fe7b825d
CHHAILENG TIM <chhailengtim2018@gmail.com>
2024-07-05 22:33:49 +0700

    finished

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -2,12 +2,15 @@ package com.example.demo.student;
 
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
+
+import jakarta.transaction.Transactional;
 
 @Component
 @Service
@@ -44,5 +47,21 @@ public class StudentService {
         }
         studentRepository.deleteById(studentId);
     }
+
+	@Transactional
+	public void updateStudent(Long studentId, String name, String email) {
+		Student student = studentRepository.findById(studentId).orElseThrow(()->new IllegalStateException("student with id " + studentId + "does not exist"));
+		if (name != null && name.length()>0 && !Objects.equals(student.getName(),name)) {
+			student.setName(name);
+		}
+		if (email != null && email.length()>0 && !Objects.equals(student.getEmail(), email)) {
+			Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+			if(studentOptional.isPresent()) {
+				throw new IllegalStateException("Email is taken");
+			}
+			student.setEmail(email);
+		}
+		
+	}
 
 }
