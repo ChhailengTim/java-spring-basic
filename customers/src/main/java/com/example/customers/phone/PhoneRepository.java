12b1ff3d3604e81749a559b3739849d6682fc46a
CHHAILENG TIM <chhailengtim2018@gmail.com>
2024-07-18 22:14:33 +0700

    update

--- customers/src/main/java/com/example/customers/phone/PhoneRepository.java
+++ customers/src/main/java/com/example/customers/phone/PhoneRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PhoneRepository extends JpaRepository<PhoneModel, Long> {
-}
+public interface PhoneRepository extends JpaRepository<PhoneModel, Long> { }
